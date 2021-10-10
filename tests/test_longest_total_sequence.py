3afecbb152c5b436278378ad4c12537828e04b2d
Quimorax <nikitademch24@gmail.com>
2021-10-10 17:02:31 +0300

    add some empty tests

---
+++ tests/test_longest_total_sequence.py
@@ -0,0 +1,6 @@
+import pytest
+from main import find_longest_total_sequence
+
+
+if __name__ == '__main__':
+    pytest.main()
---
+++ tests/test_main.py
@@ -0,0 +1,6 @@
+import pytest
+from main import main
+
+
+if __name__ == '__main__':
+    pytest.main()
