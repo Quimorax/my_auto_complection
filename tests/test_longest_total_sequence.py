7951364dca2b837481cc1d62c1b63d3b38fa726a
Quimorax <nikitademch24@gmail.com>
2021-10-12 22:04:20 +0300

    Write simple tests

--- tests/test_longest_total_sequence.py
+++ tests/test_longest_total_sequence.py
@@ -1,5 +1,21 @@
 import pytest
+
 from main import find_longest_total_sequence
+
+
+def test_with_small_words():
+    words = ('how', 'wow')
+    assert find_longest_total_sequence(*words) == 2
+
+
+def test_middle_len_words():
+    words = ('accurate', 'accountant')
+    assert find_longest_total_sequence(*words) == 7
+
+
+def test_biggest_words():
+    words = ('prescription', 'psychological')
+    assert find_longest_total_sequence(*words) == 4
 
 
 if __name__ == '__main__':
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,24 @@
 import pytest
-from main import main
+
+from main import main, find_longest_total_sequence
+
+
+def test_small_words():
+    user_word, word_user_mean = 'acual', 'actual'
+    similarity = len(word_user_mean) / find_longest_total_sequence(user_word, word_user_mean)
+    assert main(user_word) == f'Maybe your mean {word_user_mean} ({similarity: .2 %})'
+
+
+def test_middle_len_words():
+    user_word, word_user_mean = 'elswher', 'elsewhere'
+    similarity = len(word_user_mean) / find_longest_total_sequence(user_word, word_user_mean)
+    assert main(user_word) == f'Maybe your mean {word_user_mean} ({similarity: .2 %})'
+
+
+def test_longest_words():
+    user_word, word_user_mean = 'somisticated', 'sophisticated'
+    similarity = len(word_user_mean) / find_longest_total_sequence(user_word, word_user_mean)
+    assert main(user_word) == f'Maybe your mean {word_user_mean} ({similarity: .2 %})'
 
 
 if __name__ == '__main__':
