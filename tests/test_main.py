039787cd073913bbf9ad470541bc5b41a21f5244
Quimorax <nikitademch24@gmail.com>
2021-10-12 22:11:15 +0300

    Fix similarity and message in tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,20 +5,20 @@ from main import main, find_longest_total_sequence
 
 def test_small_words():
     user_word, word_user_mean = 'acual', 'actual'
-    similarity = len(word_user_mean) / find_longest_total_sequence(user_word, word_user_mean)
-    assert main(user_word) == f'Maybe your mean {word_user_mean} ({similarity: .2 %})'
+    similarity = find_longest_total_sequence(user_word, word_user_mean) / len(word_user_mean)
+    assert main(user_word) == f'Maybe you mean {word_user_mean!r} ({similarity:.2%})?'
 
 
 def test_middle_len_words():
     user_word, word_user_mean = 'elswher', 'elsewhere'
-    similarity = len(word_user_mean) / find_longest_total_sequence(user_word, word_user_mean)
-    assert main(user_word) == f'Maybe your mean {word_user_mean} ({similarity: .2 %})'
+    similarity = find_longest_total_sequence(user_word, word_user_mean) / len(word_user_mean)
+    assert main(user_word) == f'Maybe you mean {word_user_mean!r} ({similarity:.2%})?'
 
 
 def test_longest_words():
     user_word, word_user_mean = 'somisticated', 'sophisticated'
-    similarity = len(word_user_mean) / find_longest_total_sequence(user_word, word_user_mean)
-    assert main(user_word) == f'Maybe your mean {word_user_mean} ({similarity: .2 %})'
+    similarity = find_longest_total_sequence(user_word, word_user_mean) / len(word_user_mean)
+    assert main(user_word) == f'Maybe you mean {word_user_mean!r} ({similarity:.2%})?'
 
 
 if __name__ == '__main__':
