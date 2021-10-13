9b0364784f8057abfe1746561fcbe0a029024601
Quimorax <nikitademch24@gmail.com>
2021-10-13 21:39:22 +0300

    Improved auto-completion accuracy, but productivity dropped just a bit

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-"""Auto-complete with dynamic program method."""
+"""Auto-complete with dynamic program method.
+
+"""
 
 __version__ = '1.0.0'
 
 import argparse
-from random import shuffle
+import pickle
 
 
 def create_parser() -> argparse.ArgumentParser:
@@ -18,22 +20,29 @@ def create_parser() -> argparse.ArgumentParser:
 
 
 def main(word: str) -> str:
-    """Main function that start project. Iterate file with most common words and finding most similar words."""
+    """Main function that start project. Iterate dictionary with most common words and finding most similar words.
+    Dictionary is small, only 3000 words, so the function fully avoids it for maximum accuracy.
+
+    Raises:
+        ValueError: If words contain not only chars.
+
+    """
     if not word.isalpha():
         raise ValueError('Word must contain only chars')
-    entry_limit = 0.75
-    path = r'C:\Users\User\PycharmProjects\my_auto_complection\most_common_words.txt'
-    with open(path, 'r') as file:
-        lines = list(map(str.strip, file))
-        shuffle(lines)  # for non-alphabet iterate
-        if word in lines:
-            return f'{word!r} is correct word'
-        for file_word in lines:
-            longest_total_sequence = find_longest_total_sequence(word, file_word)
-            similarity = longest_total_sequence / max((len(word), len(file_word)))
-            if similarity >= entry_limit:
-                return f'Maybe you mean {file_word!r} ({similarity:.2%})?'
-    return f'No one word {word!r}'
+    entry_limit = 0.70
+    words = get_dict(r'C:\Users\User\PycharmProjects\my_auto_complection\words.pickle')
+    for compare_word in words:
+        longest_total_sequence = find_longest_total_sequence(word, compare_word)
+        similarity = longest_total_sequence / max((len(word), len(compare_word)))
+        if similarity >= entry_limit:
+            words[compare_word] = similarity
+    words = sorted(words.items(), key=lambda x: x[1], reverse=True)
+    most_similar_word, similarity = words[0][0], words[0][1]
+    if similarity < entry_limit:
+        return f'No one word {word!r}'
+    if similarity == 1:  # 100 %
+        return f'{word!r} is correct word'
+    return f'Maybe you mean {most_similar_word!r} ({similarity:.2%})?'
 
 
 def find_longest_total_sequence(user_word: str, file_word: str) -> int:
@@ -46,6 +55,17 @@ def find_longest_total_sequence(user_word: str, file_word: str) -> int:
             else:
                 matrix[i][j] = max(matrix[i - 1][j], matrix[i][j - 1])
     return max(similarity for list_ in matrix for similarity in list_)
+
+
+def save_dict(filename: str, words: dict) -> None:
+    with open(filename, 'wb') as file:
+        pickle.dump(words, file)
+
+
+def get_dict(filename: str) -> dict:
+    with open(filename, 'rb') as file:
+        words = pickle.load(file)
+    return words
 
 
 if __name__ == '__main__':
